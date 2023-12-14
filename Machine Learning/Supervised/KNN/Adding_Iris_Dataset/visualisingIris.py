19f92b24a805c91d3270e9434fdacae82085ffb2
Nosipho Donkrag <577354@student.belgiumcampus.ac.za>
2023-12-15 01:18:56 +0200

    removing panda lib

--- Machine Learning/Supervised/KNN/Adding_Iris_Dataset/visualisingIris.py
+++ Machine Learning/Supervised/KNN/Adding_Iris_Dataset/visualisingIris.py
@@ -7,14 +7,14 @@ X, y = iris.data, iris.target
 
 
 
-# Plot the data
-plt.figure(figsize=(8, 6))
+# Plot the data 
+plt.figure(figsize=(8, 6)) 
 
 # Scatter plot for Sepal Length vs Sepal Width
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis', edgecolors='k', s=80, label='Sepal')
+plt.scatter(X[:, 0], X[:, 1], c=y, s=80, label='Sepal', edgecolors='k')
 
 # Scatter plot for Petal Length vs Petal Width
-plt.scatter(X[:, 2], X[:, 3], c=y, cmap='viridis', marker='s', edgecolors='k', s=80, label='Petal')
+plt.scatter(X[:, 2], X[:, 3], c=y, s=80, label='Petal',  marker='s', edgecolors='k')
 
 # Set labels and title
 plt.xlabel('Length (cm)')
@@ -26,3 +26,4 @@ plt.legend()
 
 # Show the plot
 plt.show() 
+
