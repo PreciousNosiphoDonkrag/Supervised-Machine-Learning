9e273810d7c0549c2fb649ca65c1bef68740c46f
Nosipho Donkrag <577354@student.belgiumcampus.ac.za>
2023-12-15 16:44:42 +0200

    code for iris

--- Machine Learning/Supervised/KNN/Adding_Iris_Dataset/KNN_mathematical_with_Iris.py
+++ Machine Learning/Supervised/KNN/Adding_Iris_Dataset/KNN_mathematical_with_Iris.py
@@ -1,7 +1,93 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 
-#load and view the iris dataset
+# The Aim of this project is to give a mathematical explanation of K-Nearest Neighbors
+
+# Load the Iris dataset
+iris = load_iris()
+X, y = iris.data, iris.target
+species_names = ["setosa", "versicolor", "virginica"]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define a class that will implement KNN
+class KNN:
+    def __init__(self, k=3):  # Constructor with default k value
+        self.k = k
+        self.model = None #store instance of knn model once its trained
+
+    def fit(self, X_train, y_train):
+        #create an instance of KNeighborClassifier class found in scikit-learn
+        self.model = KNeighborsClassifier(n_neighbors=self.k)
+        
+        #calling fit on the knn instance creates a KNeighborsClassifier
+        #model with specified number of neighbors and fits it to
+        #the training data
+        self.model.fit(X_train, y_train)
+
+    #predict returns an array because in scikit-learn it is designed to
+    #return a prediction for multiple values
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+    def evaluate_accuracy(self, X_test, y_test):
+        y_pred = self.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        return accuracy
+
+    #Lets map the species name to the integers of the classes
+    def map_species_name(self, class_Name):
+        Name = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+        return Name[class_Name]
+    
+# Instantiate KNN classifier
+knn_classifier = KNN()
+
+# Fit the classifier on the training data
+knn_classifier.fit(X_train, y_train)
+
+#Add an input point to predict
+#notice that the input point is a 2D aaray instead of a 1D array with 2 elements
+#This is done to maintain consistency because the predict function
+#from scikit-learn expects a 2D array
+input_point = np.array([[5.5, 3.0,  2.0, 0.3]]) #adjust this point to see predictions
+predict_class = knn_classifier.predict(input_point)
+species_Name = knn_classifier.map_species_name(predict_class[0])
+print(f"The predicted class for the provided input is: {species_Name}")
+
+# Evaluate the accuracy on the test set
+accuracy = knn_classifier.evaluate_accuracy(X_test, y_test)
+print(f"Accuracy: {accuracy:.2f}")
+
+
+
+# Visualization
+ax = plt.subplot()
+ax.grid(True, color="#323232")
+ax.set_facecolor("#FFFFFF")
+ax.figure.set_facecolor("#FFFFFF")
+ax.tick_params(axis="x", color="black")
+ax.tick_params(axis="y", color="black")
+
+# Plot existing points
+colors = ["#02C04A", "#FF0000", "#0000FF"]
+for i in range(3):
+    indices = np.where(y == i)
+    ax.scatter(X[indices, 0], X[indices, 1], color=colors[i], s=60, label=f"Class {species_names[i]}")
+
+
+#plot the input point to predict
+ax.scatter(input_point[0, 0], input_point[0, 1], color='black', marker='*', s=200, label=f"Input Point\nPredicted Class: {species_Name}")
+
+ax.set_xlabel(iris.feature_names[0])
+ax.set_ylabel(iris.feature_names[1])
+
+# Show the plot
+plt.legend()
+plt.show()
+
